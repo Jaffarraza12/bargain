7f3937d61b718b6eab604e7110e036d79e0eccac
Appertunity <info@appertunity.net>
2020-01-31 08:40:38 -0600

    as

---
+++ live8005786110/_config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+/*********************************************************************************
+* LiveZilla config.php
+*
+* Copyright 2016 LiveZilla GmbH
+* All rights reserved.
+* LiveZilla is a registered trademark.
+*
+********************************************************************************/
+
+$_CONFIG['gl_pr_cr'] = '1542018891';
+$_CONFIG['gl_lzid'] = '64870032ba';
+$_CONFIG['b64'] = false;
+
+$_CONFIG[0]['gl_db_host'] = 'localhost';
+$_CONFIG[0]['gl_db_user'] = 'bargain_livechat';
+$_CONFIG[0]['gl_db_ext'] = 'mysqli';
+$_CONFIG[0]['gl_db_eng'] = 'MyISAM';
+$_CONFIG[0]['gl_db_pass'] = 'l4b6v5giXept';
+$_CONFIG[0]['gl_db_name'] = 'bargain_livechat';
+$_CONFIG[0]['gl_db_prefix'] = '';
+
+?>
